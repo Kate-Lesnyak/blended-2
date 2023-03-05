6347ff84f9f822ba4f84fe4dedebc8f7bdc76b5d
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-03-05 15:12:46 +0200

    update task 1

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -4,7 +4,36 @@ import { FiSearch } from 'react-icons/fi';
 import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
 
 export class SearchForm extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = e => {
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.value);
+    this.setState({ value: '' });
+  };
+
   render() {
-    return <h2>SearchForm</h2>;
+    const { value } = this.state;
+    return (
+      <SearchFormStyled onSubmit={this.handleSubmit}>
+        <FormBtn type="submit">
+          <FiSearch size="16px" />
+        </FormBtn>
+        <InputSearch
+          onChange={this.handleChange}
+          value={value}
+          placeholder="What do you want to write?"
+          name="search"
+          required
+          autoFocus
+        />
+      </SearchFormStyled>
+    );
   }
 }
--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -4,9 +4,28 @@ import * as ImageService from 'service/image-service';
 import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 
 export class Gallery extends Component {
+  state = {
+    query: '',
+    page: 1,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if (query !== prevState.query || page !== prevState.page) {
+      ImageService.getImages(query, page).then(data => {
+        console.log(data);
+      });
+    }
+  }
+
+  handleSubmit = query => {
+    this.setState({ query });
+  };
+
   render() {
     return (
       <>
+        <SearchForm onSubmit={this.handleSubmit} />
         <Text textAlign="center">Sorry. There are no images ... 😭</Text>
       </>
     );
