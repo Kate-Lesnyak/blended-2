9233821e65780a64068015347d4881bd5e498a14
Midland707 <rus14job@ukr.net>
2023-03-05 15:51:41 +0200

    gallary+button

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -7,26 +7,52 @@ export class Gallery extends Component {
   state = {
     query: '',
     page: 1,
+    images: [],
+    showBtn: false,
   };
 
   componentDidUpdate(prevProps, prevState) {
     const { query, page } = this.state;
     if (query !== prevState.query || page !== prevState.page) {
-      ImageService.getImages(query, page).then(data => {
-        console.log(data);
+      ImageService.getImages(query, page).then(({ photos, total_results }) => {
+        this.setState(prevState => ({
+          images: [...prevState.images, ...photos],
+          showBtn: page < Math.ceil(total_results / 15),
+        }));
+        console.log(photos);
       });
     }
   }
 
   handleSubmit = query => {
-    this.setState({ query });
+    this.setState({ query, images: [], page: 1, showBtn: false });
+  };
+
+  onClickButton = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
 
   render() {
     return (
       <>
         <SearchForm onSubmit={this.handleSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <Grid>
+          {this.state.images.map(({ alt, avg_color, id, src: { large } }) => {
+            return (
+              <GridItem key={id}>
+                <CardItem color={avg_color}>
+                  <img src={large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            );
+          })}
+        </Grid>
+        {this.state.showBtn && (
+          <Button onClick={this.onClickButton}>Load more</Button>
+        )}
+        {/* <Text textAlign="center">Sorry. There are no images ... 😭</Text> */}
       </>
     );
   }
