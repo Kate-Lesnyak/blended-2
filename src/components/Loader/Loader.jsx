c3266ffeb79aa83f266f471bd3a2eda053a61269
Serhii Karpus <49605599+Siren1790@users.noreply.github.com>
2023-03-05 16:27:43 +0200

    Add Spiner

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Blocks } from 'react-loader-spinner';
+import { Backdrop } from './Loader.styled';
+
+export const Loader = () => {
+  return (
+    <Backdrop>
+      <Blocks
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+      />
+    </Backdrop>
+  );
+};
