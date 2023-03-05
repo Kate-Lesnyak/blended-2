3a993af7003b1cd8091b40380bdab9be0f3a1d19
serhiikravchak <105275364+serhiikravchak@users.noreply.github.com>
2023-03-05 15:58:12 +0200

    todos added

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -3,7 +3,33 @@ import { nanoid } from 'nanoid';
 import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
+  state = {
+    todos: [],
+ }
+  onSubmit = (text) => {
+    const todo = {
+      text,
+      id: nanoid()
+    }
+    this.setState(prevState=>({todos:[...prevState.todos,todo]}))
+  }
+
+  deleteTodo = (id) => {
+    this.setState(prevState=>({todos: prevState.todos.filter(todo=>todo.id !==id)}))
+  } 
+
+
+  
   render() {
-    return <Text>Todos</Text>;
+    return <>
+      <SearchForm onSubmit={this.onSubmit} />
+      <Grid>
+        {this.state.todos.map((todo,index) => {
+         return <GridItem key={todo.id}>
+           <Todo text={todo.text} counter={index + 1} deleteTodo={this.deleteTodo} id={todo.id} />
+          </GridItem>;
+        })}
+      </Grid>
+    </>;
   }
 }
